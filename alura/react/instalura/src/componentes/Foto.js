e935b4f1ff7542a8f71662c4ff878bf91212ba6e
Wesley Egberto <wesleyegberto@gmail.com>
2017-04-16 00:58:05 -0300

    React course 2: added fetch

--- alura/react/instalura/src/componentes/Foto.js
+++ alura/react/instalura/src/componentes/Foto.js
@@ -17,27 +17,21 @@ class FotoAtualizacoes extends Component {
 class FotoInfo extends Component {
 	render(){
 		return (
-			<div className="foto-in fo">
+			<div className="foto-info">
 				<div className="foto-info-likes">
-					<a href="#">alots_ssa</a>, <a href="#">rafael_rollo</a> curtiram
+					{this.props.foto.likers.map(liker => <a href="#">{liker.loginUsuario}</a>)} curtiram
 				</div>
 				<p className="foto-info-legenda">
-					<a className="foto-info-autor">autor </a>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+					<a className="foto-info-autor">{this.props.foto.loginUsuario} </a>
+					{this.props.foto.comentario}
 				</p>
 				<ul className="foto-info-comentarios">
-					<li className="comentario">
-						<a className="foto-info-autor">seguidor </a>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-					</li>
-					<li className="comentario">
-						<a className="foto-info-autor">seguidor </a>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-					</li>
-					<li className="comentario">
-						<a className="foto-info-autor">seguidor </a>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-					</li>
+					{this.props.foto.comentarios.map(comentario => (
+						<li className="comentario">
+							<a className="foto-info-autor">{comentario.loginUsuario} </a>
+							{comentario.texto}
+						</li>
+					))}
 				</ul>
 			</div>
 		);
@@ -49,10 +43,10 @@ class FotoHeader extends Component {
 		return (
 			<header className="foto-header">
 				<figure className="foto-usuario">
-					<img src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg" alt="foto do usuario"/>
-					<figcaption className="foto-usuario"><a href="#">alots</a></figcaption>
+					<img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
+					<figcaption className="foto-usuario"><a href="#">{this.props.foto.loginUsuario}</a></figcaption>
 				</figure>
-				<time className="foto-data">03/10/2016 20:13</time>
+				<time className="foto-data">{this.props.foto.horario}</time>
 			</header>
 		);
 	}
@@ -62,10 +56,10 @@ export default class Foto extends Component {
 	render(){
 		return (
 			<div className="foto">
-				<FotoHeader/>
-				<img alt="foto" className="foto-src" src="https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2"/>
-				<FotoInfo/>
-				<FotoAtualizacoes/>
+				<FotoHeader foto={this.props.foto}/>
+				<img alt="foto" className="foto-src" src={this.props.foto.urlFoto} />
+				<FotoInfo foto={this.props.foto}/>
+				<FotoAtualizacoes foto={this.props.foto}/>
 			</div>
 		);
 	}
--- alura/react/instalura/src/componentes/Timeline.js
+++ alura/react/instalura/src/componentes/Timeline.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import Foto from './Foto';
 
 export default class Timeline extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			fotos: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:8080/api/public/fotos/rafael')
+			.then(response => response.json())
+			.then(fotos => this.setState({ fotos: fotos }));
+	}
+
 	render(){
 		return (
 		<div className="fotos container">
-			<Foto/>
-			<Foto/>
+			{this.state.fotos.map(ft => <Foto foto={ft} />)}
 		</div>
 		);
 	}
