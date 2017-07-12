8d55e8e3042770e144a96b80697a3fd6a4fb631c
Wesley Egberto <wesleyegberto@gmail.com>
2017-07-11 23:28:13 -0300

    React: Continuacao do curso

--- alura/react/instalura/src/componentes/Foto.js
+++ alura/react/instalura/src/componentes/Foto.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Pubsub from 'pubsub-js';
 
 class FotoAtualizacoes extends Component {
-	render(){
+	constructor() {
+		super();
+		this.state = {likeada : this.props.foto.likeada};
+	}
+	like(event) {
+		event.preventDefault();
+		let token = localStorage.getItem('auth-token');
+		fetch(`http://localhost:8080/api/fotos/${this.props.foto.id}/like?X-AUTH-TOKEN=${token}`, {method: 'POST'})
+			.then(response => {
+				if (response.ok) {
+					response.json();
+				} else {
+					throw new Error('Não foi possível realizar o like da foto.');
+				}
+			})
+			.then(liker => {
+				this.setState({likeada : !this.state.likeada});
+				Pubsub.publish('atualiza-liker',{fotoId:this.props.foto.id,liker});
+			});
+	}
+	
+	render() {
 		return (
 			<section className="fotoAtualizacoes">
-				<a href="#" className="fotoAtualizacoes-like">Likar</a>
+				<a onClick={this.link.bind(this)} className={this.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
 				<form className="fotoAtualizacoes-form">
 					<input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
 					<input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
@@ -16,11 +38,29 @@ class FotoAtualizacoes extends Component {
 }
 
 class FotoInfo extends Component {
+	constructor(props){
+		super(props);
+		this.state = {likers : this.props.foto.likers};
+	}
+			
+	componentWillMount(){
+		Pubsub.subscribe('atualiza-liker',(topico, infoLiker) => {
+			const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login);
+			if(possivelLiker === undefined) {
+				const novosLikers = this.state.likers.concat(infoLiker.liker);
+				this.setState({likers:novosLikers});
+			} else {
+				const novosLiker = this.state.filter(liker => lilker.login !== infoLiker.liker.login);
+				this.setState({likers:novosLikers});
+			}
+		});
+	}
+			
 	render(){
 		return (
 			<div className="foto-info">
 				<div className="foto-info-likes">
-					{this.props.foto.likers.map(liker => <Link key={liker.loginUsuario} to={`/timeline/${liker.loginUsuario}`}>{liker.loginUsuario}</Link>)} curtiram
+					{this.state.likers.map(liker => <Link key={liker.loginUsuario} to={`/timeline/${liker.loginUsuario}`}>{liker.loginUsuario}</Link>)} curtiram
 				</div>
 				<p className="foto-info-legenda">
 					<Link className="foto-info-autor" to={`/timeline/${this.props.foto.loginUsuario}`}>{this.props.foto.loginUsuario}</Link>
