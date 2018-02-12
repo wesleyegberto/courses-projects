1a49964cbd3d4a7b05355e14ee217043f4310591
Wesley Egberto <wesleyegberto@gmail.com>
2018-02-12 01:16:15 -0200

    JS 3: removed JS scripts to used builded

--- alura/js/aluraframe/client/js/app/view/MensagemView.js
+++
@@ -1,9 +0,0 @@
-class MensagemView extends View {
-  constructor(elemento) {
-    super(elemento);
-  }
-
-  template(model) {
-    return model && model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '';
-  }
-}
